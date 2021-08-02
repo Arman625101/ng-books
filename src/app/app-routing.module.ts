42b945f052f4b43a6c7f71b40b095576255aa8c5
Arman625101 <vip.arman2011@gmail.com>
2021-08-03 00:32:52 +0400

    added routing and 3 main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { GenresComponent } from './genres/genres.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: BooksComponent },
+  { path: 'authors', component: AuthorsComponent },
+  { path: 'genres', component: GenresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { GenresComponent } from './genres/genres.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AuthorsComponent,
+    GenresComponent
   ],
   imports: [
     BrowserModule,
